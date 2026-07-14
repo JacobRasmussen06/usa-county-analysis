f5bde09a242b6a9d5164ef5a6b37dc4e254f183f
Jacob Rasmussen <jakerbaker06@gmail.com>
2026-07-14 12:16:16 -0500

    Downloaded an initial ACS dataset.

---
+++ scripts/01_download_data.R
@@ -0,0 +1,37 @@
+###################################################################
+#
+# 01_download_data.R
+#
+#
+#
+#
+#
+#
+###################################################################
+
+library(tidycensus)
+library(tidyverse)
+
+
+census_api_key("0284737ce531e26d237dfd8af2a15149598f27ce", install = TRUE)
+
+acs_variables <- c(
+  population = "B01003_001",
+  median_age = "B01002_001",
+  median_income = "B19013_001",
+  poverty = "B17001_002",
+  unemployment = "B23025_005",
+  bachelors_degree = "B15003_022",
+  median_home_value = "B25077_001",
+  median_rent = "B25064_001"
+)
+
+
+county_data <- get_acs(
+  geography = "county",
+  variables = acs_variables,
+  year = 2023,
+  survey = "acs5"
+)
+
+write_csv(county_data, "data/raw/acs_initialpull.csv")
